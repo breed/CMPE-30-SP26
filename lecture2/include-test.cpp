62ecd5495eb0900d0a9a860e1c02a39beb70883d
ben reed <bcr33d@gmail.com>
2026-01-27 21:54:51 -0800

    lecture1 and lecture2

---
+++ lecture2/include-test.cpp
@@ -0,0 +1,19 @@
+// NEVER EVER EVER WRITE CODE LIKE THIS!!!!
+// THIS IS JUST TO EXPERIMENT WITH include
+
+#include <print>
+
+int main() {
+    std::println(
+#include "string1.h"
+#include "string1.h"
+#include "string1.h"
+#include "string2"
+#include "string2"
+#include "string2"
+#include "string3"
+#include "string3"
+#include "string3"
+#include "string3"
+);
+}
---
+++ lecture2/test-string-literal-concat.cpp
@@ -0,0 +1,7 @@
+#include <print>
+
+int main() {
+    std::println("C++ will concatonate literal" "strings that are only"
+"separated by"
+                "whitespace");
+}
